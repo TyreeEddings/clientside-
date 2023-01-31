e39e4c828c93490db86f6536dbd9a7f66ade6f39
TyreeEddings <97616579+TyreeEddings@users.noreply.github.com>
2023-01-31 08:56:24 -0600

    Update handson2-2.js
    Signed-off-by: TyreeEddings <97616579+TyreeEddings@users.noreply.github.com>

--- Module2/handson2-2/handson2-2.js
+++ Module2/handson2-2/handson2-2.js
@@ -1,1 +1,23 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-02
+      Application to test for completed form
+      Author: 
+      Date:   
+      Filename: handson2-2.js
+ */
+ 
+document.getElementById("submit").addEventListener("click", verifyForm);
 
+
+function verifyForm(){
+      let name = document.getElementById("name").value;
+      let email = document.getElementById("email").value;
+      let phone = document.getElementById("phone").value;
+
+      if (name && email && phone) {
+      window.alert("Thank you!");
+      } else {
+      window.alert("Please fill in all fields");
+      }
+}
