d58bf2e574f6a5a80470442994af53db0aca0702
TyreeEddings <97616579+TyreeEddings@users.noreply.github.com>
2023-01-31 08:46:18 -0600

    Update handson2-1.js
    Signed-off-by: TyreeEddings <97616579+TyreeEddings@users.noreply.github.com>

--- Module2/handson2-1/handson2-1.js
+++ Module2/handson2-1/handson2-1.js
@@ -1,1 +1,26 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-01
+      Celsius <-> Farenheit Coverter
+      Author: 
+      Date:   
+      Filename: project02-01.js
+ */
 
+
+function FHtoCL(fh) {
+      return (fh - 32) * (5/9)
+}
+      
+function CLtoFH(cl) {
+      return ((cl * (9/5)) + 32)
+}
+
+document.getElementById("cValue").onchange = function(){
+      let cDegree = document.getElementById("cValue").value;
+      document.getElementById("fValue").value = CLtoFH(cDegree);
+}
+document.getElementById("fValue").onchange = function(){
+      let fDegree = document.getElementById("fValue").value;
+      document.getElementById("cValue").value = FHtoCL(fDegree);
+}
