b3d0890189f728740f05be43192f6e92541f0ced
TyreeEddings <97616579+TyreeEddings@users.noreply.github.com>
2023-02-01 09:18:26 -0600

    Update checkout.js
    Signed-off-by: TyreeEddings <97616579+TyreeEddings@users.noreply.github.com>

--- module1/case project/checkout/checkout.js
+++ module1/case project/checkout/checkout.js
@@ -1,150 +1,120 @@
-// https://github.com/jasonmoo/t.js
-(function(){function c(a){this.t=a}function l(a,b){for(var e=b.split(".");e.length;){if(!(e[0]in a))return!1;a=a[e.shift()]}return a}function d(a,b){return a.replace(h,function(e,a,i,f,c,h,k,m){var f=l(b,f),j="",g;if(!f)return"!"==i?d(c,b):k?d(m,b):"";if(!i)return d(h,b);if("@"==i){e=b._key;a=b._val;for(g in f)f.hasOwnProperty(g)&&(b._key=g,b._val=f[g],j+=d(c,b));b._key=e;b._val=a;return j}}).replace(k,function(a,c,d){return(a=l(b,d))||0===a?"%"==c?(new Option(a)).innerHTML.replace(/"/g,"&quot;"):
-a:""})}var h=/\{\{(([@!]?)(.+?))\}\}(([\s\S]+?)(\{\{:\1\}\}([\s\S]+?))?)\{\{\/\1\}\}/g,k=/\{\{([=%])(.+?)\}\}/g;c.prototype.render=function(a){return d(this.t,a)};window.t=c})();
-// end of 't';
+"use strict";
 
-Number.prototype.to_$ = function () {
-  return "$" + parseFloat( this ).toFixed(2);
-};
-String.prototype.strip$ = function () {
-  return this.split("$")[1];
-};
+var input = document.getElementById('input'), // input/output button
+  number = document.querySelectorAll('.numbers div'), // number buttons
+  operator = document.querySelectorAll('.operators div'), // operator buttons
+  result = document.getElementById('result'), // equal button
+  clear = document.getElementById('clear'), // clear button
+  resultDisplayed = false; // flag to keep an eye on what output is displayed
 
-var app = {
+// adding click handlers to number buttons
+for (var i = 0; i < number.length; i++) {
+  number[i].addEventListener("click", function(e) {
 
-  shipping : 5.00,
-  products : [
-     {
-        "name" : "Standard Coffee",
-        "price" : "19.99",
-        "desc" : "Start your day off right"
-      },
-      {
-        "name" : "sample",
-        "price" : "9.99",
-        
-        "desc" : "You decide"
-      },
-      {
-        "name" : "Gear",
-        "price" : "49.99",
-        
-        "desc" : "Level up your coffee experience "
-      },
-      {
-        "name" : "Gear PLUS",
-       
-        "price" : "99.99",
-        "desc" : "Don't get tired!"
-      }
-    ],
+    // storing current input string and its last character in variables - used later
+    var currentString = input.innerHTML;
+    var lastChar = currentString[currentString.length - 1];
 
-
-  removeProduct: function () {
-    "use strict";
-
-    var item = $(this).closest(".shopping-cart--list-item");
-
-    item.addClass("closing");
-    window.setTimeout( function () {
-      item.remove();
-      app.updateTotals();
-    }, 500); // Timeout for css animation
-  },
-
-  addProduct: function () {
-    "use strict";
-
-    var qtyCtr = $(this).prev(".product-qty"),
-        quantity = parseInt(qtyCtr.html(), 10) + 1;
-
-    app.updateProductSubtotal(this, quantity);
-  },
-
-  subtractProduct: function () {
-    "use strict";
-
-    var qtyCtr = $(this).next(".product-qty"),
-        num = parseInt(qtyCtr.html(), 10) - 1,
-        quantity = num <= 0 ? 0 : num;
-
-    app.updateProductSubtotal(this, quantity);
-  },
-
-  updateProductSubtotal: function (context, quantity) {
-    "use strict";
-
-    var ctr = $(context).closest(".product-modifiers"),
-        productQtyCtr = ctr.find(".product-qty"),
-        productPrice = parseFloat(ctr.data("product-price")),
-        subtotalCtr = ctr.find(".product-total-price"),
-        subtotalPrice = quantity * productPrice;
-
-    productQtyCtr.html(quantity);
-    subtotalCtr.html( subtotalPrice.to_$() );
-
-    app.updateTotals();
-  },
-
-  updateTotals: function () {
-    "use strict";
-
-    var products = $(".shopping-cart--list-item"),
-        subtotal = 0,
-        shipping;
-
-    for (var i = 0; i < products.length; i += 1) {
-      subtotal += parseFloat( $(products[i]).find(".product-total-price").html().strip$() );
+    // if result is not diplayed, just keep adding
+    if (resultDisplayed === false) {
+      input.innerHTML += e.target.innerHTML;
+    } else if (resultDisplayed === true && lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
+      // if result is currently displayed and user pressed an operator
+      // we need to keep on adding to the string for next operation
+      resultDisplayed = false;
+      input.innerHTML += e.target.innerHTML;
+    } else {
+      // if result is currently displayed and user pressed a number
+      // we need clear the input string and add the new input to start the new opration
+      resultDisplayed = false;
+      input.innerHTML = "";
+      input.innerHTML += e.target.innerHTML;
     }
 
-    shipping = (subtotal > 0 && subtotal < (100 / 1.06)) ? app.shipping : 0;
+  });
+}
 
-    $("#subtotalCtr").find(".cart-totals-value").html( subtotal.to_$() );
-    $("#taxesCtr").find(".cart-totals-value").html( (subtotal * 0.06).to_$() );
-    $("#totalCtr").find(".cart-totals-value").html( (subtotal * 1.06 + shipping).to_$() );
-    $("#shippingCtr").find(".cart-totals-value").html( shipping.to_$() );
-  },
+// adding click handlers to number buttons
+for (var i = 0; i < operator.length; i++) {
+  operator[i].addEventListener("click", function(e) {
 
-  attachEvents: function () {
-    "use strict";
+    // storing current input string and its last character in variables - used later
+    var currentString = input.innerHTML;
+    var lastChar = currentString[currentString.length - 1];
 
-    $(".product-remove").on("click", app.removeProduct);
-    $(".product-plus").on("click", app.addProduct);
-    $(".product-subtract").on("click", app.subtractProduct);
-  },
-
-  setProductImages: function () {
-    "use strict";
-
-    var images = $(".product-image"),
-        ctr,
-        img;
-
-    for (var i = 0; i < images.length; i += 1) {
-      ctr = $(images[i]),
-      img = ctr.find(".product-image--img");
-
-      ctr.css("background-image", "url(" + img.attr("src") + ")");
-      img.remove();
-    }
-  },
-
-  renderTemplates: function () {
-    "use strict";
-
-    var products = app.products,
-        content = [],
-        template = new t( $("#shopping-cart--list-item-template").html() );
-
-    for (var i = 0; i < products.length; i += 1) {
-      content[i] = template.render(products[i]);
+    // if last character entered is an operator, replace it with the currently pressed one
+    if (lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
+      var newString = currentString.substring(0, currentString.length - 1) + e.target.innerHTML;
+      input.innerHTML = newString;
+    } else if (currentString.length == 0) {
+      // if first key pressed is an opearator, don't do anything
+      console.log("enter a number first");
+    } else {
+      // else just add the operator pressed to the input
+      input.innerHTML += e.target.innerHTML;
     }
 
-    $("#shopping-cart--list").html(content.join(""));
+  });
+}
+
+// on click of 'equal' button
+result.addEventListener("click", function() {
+
+  // this is the string that we will be processing eg. -10+26+33-56*34/23
+  var inputString = input.innerHTML;
+
+  // forming an array of numbers. eg for above string it will be: numbers = ["10", "26", "33", "56", "34", "23"]
+  var numbers = inputString.split(/\+|\-|\×|\÷/g);
+
+  // forming an array of operators. for above string it will be: operators = ["+", "+", "-", "*", "/"]
+  // first we replace all the numbers and dot with empty string and then split
+  var operators = inputString.replace(/[0-9]|\./g, "").split("");
+
+  console.log(inputString);
+  console.log(operators);
+  console.log(numbers);
+  console.log("----------------------------");
+
+  // now we are looping through the array and doing one operation at a time.
+  // first divide, then multiply, then subtraction and then addition
+  // as we move we are alterning the original numbers and operators array
+  // the final element remaining in the array will be the output
+
+  var divide = operators.indexOf("÷");
+  while (divide != -1) {
+    numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
+    operators.splice(divide, 1);
+    divide = operators.indexOf("÷");
   }
 
-};
+  var multiply = operators.indexOf("×");
+  while (multiply != -1) {
+    numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
+    operators.splice(multiply, 1);
+    multiply = operators.indexOf("×");
+  }
 
-app.renderTemplates();
-app.setProductImages();
-app.attachEvents();
+  var subtract = operators.indexOf("-");
+  while (subtract != -1) {
+    numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
+    operators.splice(subtract, 1);
+    subtract = operators.indexOf("-");
+  }
+
+  var add = operators.indexOf("+");
+  while (add != -1) {
+    // using parseFloat is necessary, otherwise it will result in string concatenation :)
+    numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
+    operators.splice(add, 1);
+    add = operators.indexOf("+");
+  }
+
+  input.innerHTML = numbers[0]; // displaying the output
+
+  resultDisplayed = true; // turning flag if result is displayed
+});
+
+// clearing the input on press of clear
+clear.addEventListener("click", function() {
+  input.innerHTML = "";
+})
