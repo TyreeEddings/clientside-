2503c066cef5f0f50c7fcf37ff2b21ea5099fc13
TyreeEddings <97616579+TyreeEddings@users.noreply.github.com>
2023-03-24 16:02:27 -0500

    Create project06-01.js
    Signed-off-by: TyreeEddings <97616579+TyreeEddings@users.noreply.github.com>

---
+++ Module6/handson6-1/project06-01.js
@@ -0,0 +1,48 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 6
+      Project 06-01
+      Author: Tyree Eddings-Monk
+      Date: 3/24/2023
+      Filename: project06-01.js
+*/
+
+let submitButton = document.getElementById("submitButton");
+
+let pwd = document.getElementById("pwd");
+
+let pwd2 = document.getElementById("pwd2");
+
+ 
+
+submitButton.addEventListener("click", function() {
+
+ 
+
+
+
+if (pwd.validity.valueMissing) {
+
+            pwd.setCustomValidity("Please fill in the password field.");
+
+     
+
+    } else if (pwd.validity.patternMismatch) {
+
+      pwd.setCustomValidity("Your password must be at least 8 characters with at least one letter and one number.");
+
+    } else {
+
+    
+
+    if (pwd.value === pwd2.value) {
+
+       
+      pwd.setCustomValidity("");
+
+    } 
+    else {
+      pwd.setCustomValidity("Your passwords must match."); //need to fix this and put it in the if (pwd.value !== pwd2.value)
+    }
+  }
+});
