eb12126f487d632ad408976e51a2f27f7a47e44a
TyreeEddings <97616579+TyreeEddings@users.noreply.github.com>
2023-04-13 20:54:34 -0500

    Update personalitytest.js
    Signed-off-by: TyreeEddings <97616579+TyreeEddings@users.noreply.github.com>

--- module1/case project/personalitytest/personalitytest.js
+++ module1/case project/personalitytest/personalitytest.js
@@ -29,3 +29,47 @@ select.addEventListener('change', (event) => {
   
   output.textContent = message;
 });
+
+ // Get the form element and the selections div
+      const form = document.querySelector('form');
+      const selectionsDiv = document.querySelector('#selections');
+
+      // Create an empty array to store the selections
+      let selections = [];
+
+      // Add an event listener to the form
+      form.addEventListener('submit', (event) => {
+        event.preventDefault(); // prevent the form from submitting
+
+        // Get all the selected checkboxes
+        const checkboxes = document.querySelectorAll('input[name="coffee"]:checked');
+
+        // Loop through the checkboxes and add their values to the selections array
+        selections = [];
+        checkboxes.forEach((checkbox) => {
+          selections.push(checkbox.value);
+        });
+
+        // Display the selections in the selections div
+        selectionsDiv.textContent = selections.join(', ');
+      });
+
+      // Add event listeners to the checkboxes to update the selections array when a checkbox is deselected
+      const checkboxInputs = document.querySelectorAll('input[name="coffee"]');
+      checkboxInputs.forEach((input) => {
+        input.addEventListener('change', () => {
+          if (!input.checked) {
+            // If the checkbox is deselected, remove its value from the selections array
+            selections = selections.filter((value) => {
+              return value !== input.value;
+            });
+
+            // Display the updated selections in the selections div
+            selectionsDiv.textContent = selections.join(', ');
+          }
+        });
+      });
+
+      // Add code to convert the selections array to a string and display it
+      const selectionsString = JSON.stringify(selections);
+      console.log(selectionsString);
